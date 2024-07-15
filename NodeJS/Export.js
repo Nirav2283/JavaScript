ddc1ce5211ef6c7d7557490fddcffd90e9ab3374
Nirav2283 <niravvaghasia95@gmail.com>
2024-07-15 09:03:37 +0530

    NodeJS

---
+++ NodeJS/Export.js
@@ -0,0 +1,10 @@
+function display(String){
+    console.log(String);
+
+}
+
+function area(r){
+    return Math.PI * r * r;
+}
+module.exports.display = display;
+module.exports.area = area;
---
+++ NodeJS/First.js
@@ -0,0 +1,3 @@
+const a = require('./Export');
+console.log(a.display("Hello"));
+console.log(a.area(10));
