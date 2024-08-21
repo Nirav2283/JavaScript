c7ca6b0367a367266e49de22bdd375739cd789f4
Nirav2283 <niravvaghasia95@gmail.com>
2024-08-21 09:27:27 +0530

    ExpressJS

--- ExpressJS/Faculty.js
+++ ExpressJS/Faculty.js
@@ -2,15 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Faculty = require('./schema');
-
-mongoose.connect("mongodb+srv://niravvaghasia:nirav@cluster0.mpmom.mongodb.net/")
+require('dotenv').config();
+mongoose.connect("mongodb+srv://"+process.env.DBUN+":"+process.env.DBPASS+"@cluster0.mpmom.mongodb.net/")
 .then(()=>{
     const app = express();
     app.use(bodyParser.urlencoded({extended: false}))
 
     //code
 
-    app.listen(3000, ()=>{
-        console.log("Runnig at 3000");
+    app.listen(process.env.PORT, ()=>{
+        console.log("Running at "+process.env.PORT);
     })
 })
